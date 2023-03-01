262ac88cda0012e8f6d4266e5ef0036cf36e6cb7
Jean Massip <jean.massip@yahoo.com>
2023-03-01 17:10:29 +0100

    Finito

--- initcontainer/main.go
+++ initcontainer/main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	fmt.Println("STARTING")
-	file, err := os.OpenFile("/cache/fichier.doc", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	message := struct {
+		Message string `bson:"message"`
+	}{
+		Message: string(os.Getenv("MESSAGE")),
+	}
+
+	fmt.Println("CONNECTING TO DB")
+	clientOptions := options.Client().ApplyURI(os.Getenv("DB_URI"))
+	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	_, err = file.Write([]byte(os.Getenv("MESSAGE")))
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("CONNECTED")
+
+	fmt.Println("WRITING IN DB")
+	coll := client.Database("TP903").Collection("message")
+	_, err = coll.InsertOne(context.Background(), message, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
--- main.go
+++ main.go
@@ -1,28 +1,52 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		message, err := os.ReadFile("/cache/fichier.doc")
+		response := struct {
+			Message string `bson:"message"`
+		}{}
+
+		fmt.Println("CONNECTING TO DB")
+		clientOptions := options.Client().ApplyURI(os.Getenv("DB_URI"))
+		client, err := mongo.Connect(context.Background(), clientOptions)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Println(err)
-		fmt.Println(message)
-
-		response := struct {
-			Message string
-		}{
-			Message: string(message),
+		err = client.Ping(context.Background(), nil)
+		if err != nil {
+			log.Fatal(err)
 		}
+		fmt.Println("CONNECTED")
+
+		fmt.Println("READING MESSAGE")
+		coll := client.Database("TP903").Collection("message")
+		cur, err := coll.Find(context.Background(), bson.M{}, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for cur.Next(context.Background()) {
+			err = cur.Decode(&response)
+			fmt.Println(response.Message)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		fmt.Println("MESSAGE READ")
 
 		res, err := json.Marshal(response)
 		if err != nil {
